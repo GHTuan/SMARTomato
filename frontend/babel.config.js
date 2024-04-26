d369e58e75df8ed4f28859ff16756576ef31532c
longtruong2604 <longtruong2604.@gmail.com>
2024-04-26 17:43:23 +0700

    Add home screen

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-      'react-native-reanimated/plugin',
-    ],
+    'nativewind/babel',
+    'react-native-reanimated/plugin',
+    // [
+    //   '@tamagui/babel-plugin',
+    //   {
+    //     components: ['tamagui'],
+    //     config: './tamagui.config.ts',
+    //     importsWhitelist: ['constants.js', 'colors.js'],
+    //     logTimings: true,
+    //     disableExtraction: process.env.NODE_ENV === 'development',
+    //   },
+    // ],
+  ],
 };
